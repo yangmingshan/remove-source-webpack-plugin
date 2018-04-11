152597710e0fbe969fe7d0a1fbdef614ce9fedc7
杨明山 <y.mingshan3@gmail.com>
2018-04-11 18:41:08 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # remove-source-webpack-plugin
-A plugin for webpack to remove the source that are not needed
+A plugin for webpack to remove the source that are not needed. Can be used with [html-webpack-inline-source-plugin](https://github.com/DustinJackson/html-webpack-inline-source-plugin) to remove inlined source.
+
+## Installation
+You can install it via [yarn](https://yarnpkg.com/) or [npm](https://npmjs.com/).
+```
+$ yarn add remove-source-webpack-plugin --dev
+$ npm install remove-source-webpack-plugin --save-dev
+```
+
+## Usage
+Require the plugin in your webpack config:
+```
+const RemoveSourceWebpackPlugin = require('remove-source-webpack-plugin');
+```
+Add the plugin to your webpack config as follows:
+```
+plugins: [
+  new RemoveSourceWebpackPlugin(/runtime.*\.js$/) // array is also accepted
+]
+```
+
+## Demo
+https://github.com/yangmingshan/react-demo
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function RemoveSourceWebpackPlugin(para) {
+  let regexList = [];
+  if (typeof para === 'string') {
+    regexList = [ new RegExp(para) ];
+  } else if (para instanceof RegExp) {
+    regexList = [ para ];
+  } else if (para instanceof Array) {
+    regexList = para.map(item => {
+      if (typeof item === 'string') {
+        return new RegExp(item);
+      } else if (item instanceof RegExp) {
+        return item;
+      } else {
+        throw new Error('remove-source-webpack-plugin parameter error');
+      }
+    });
+  } else {
+    throw new Error('remove-source-webpack-plugin parameter error');
+  }
+  this.regexList = regexList;
+}
+
+RemoveSourceWebpackPlugin.prototype.apply = function(compiler) {
+  const remover = (compilation, callback) => {
+    const keys = Object.keys(compilation.assets);
+    this.regexList.forEach(regex => {
+      const source = keys.find(key => regex.test(key));
+      if (source) delete compilation.assets[source];
+    });
+    if (callback) callback();
+  };
+
+  if (compiler.hooks) {
+    compiler.hooks.emit.tap('remove-source-webpack-plugin', remover);
+  } else {
+    compiler.plugin('emit', remover);
+  }
+};
+
+module.exports = RemoveSourceWebpackPlugin;
